8285689ed31da332701d462db5a41f3990a98a5c
shreyashrangari08-gif <shreyashrangari08@gmail.com>
2026-05-27 17:45:36 +0530

    Update course.js

--- course.js
+++ course.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    description: String,
-    videoUrl: String,
-    instructor: String,
-    quiz: [{ 
-        question: String, 
-        options: [String], 
-        answer: String 
-    }]
+    shortDesc: String,
+    longDesc: String,
+    imageUrl: String,
+    videoUrl: String, // YouTube Embed Link
+    source: String
 });
 
 module.exports = mongoose.models.Course || mongoose.model('Course', courseSchema);

86a22c9c41b1256c088787db93345f79d00ae4b7
shreyashrangari08-gif <shreyashrangari08@gmail.com>
2026-06-07 22:15:40 +0530

    Update user.js

--- user.js
+++ user.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    email: { type: String, required: true, unique: true }
-});
+    email: { type: String, required: true, unique: true },
+    certificateEarned: { type: Boolean, default: false }, // Naya field
+    courseProgress: { type: Number, default: 0 }           // Naya field
+}, { timestamps: true }); // Yeh line important hai - ye apne aap 'createdAt' date add karegi
 
 module.exports = mongoose.model('User', userSchema);
